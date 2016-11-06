ed36c9f82c6b9f554752d34e9b066660a1821f4c
Tom Tangelder <tomtangelder@outlook.com>
2016-11-05 20:23:27 -0600

    Updating site with background gradient, and now populating weather info

--- local_weather.js
+++ local_weather.js
@@ -1,4 +1,4 @@
-var weather = {}
+var weather_info = {}
 var myLocation = {}
 var weather_URL = "https://api.wunderground.com/api/9e34a9a814ceadcf/conditions/q/"
 
@@ -16,18 +16,31 @@ function getLocalWeather(func){
 
 var updateWeather = function(json) {
 	console.log("Updating the Weather")
-	 weather = {
+	 weather_info = {
 	 	'temperature_f': json.current_observation.temp_f,
-	 	'temperature_c': json.current_observation.temp_c
+	 	'temperature_c': json.current_observation.temp_c,
+	 	'feelslike_f': json.current_observation.feelslike_f,
+	 	'feelslike_c': json.current_observation.feelslike_c,
+	 	'weather': json.current_observation.weather,
+	 	'weather_icon_url': json.current_observation.icon_url,
+	 	'precip_today_in': json.current_observation.precip_today_in, 
+	 	'wind_mph': json.current_observation.wind_mph,
+	 	'city': json.current_observation.display_location.city,
+	 	'state': json.current_observation.display_location.state,
+	 	'state_name': json.current_observation.display_location.state_name,
+	 	'country': json.current_observation.display_location.country
 	 };
 	console.log(json)
-	console.log(weather)
+	console.log(weather_info)
 	updateView()	
-	//$('.weather').html(weather.temperature_f);
 };
 
 function updateView(){
-	$('.weather').html(weather.temperature_f);
+	$('.weather').html(weather_info.temperature_f + " &#x2109");
+	$('.icon').html("<img src=" + weather_info.weather_icon_url + ">");
+	$('.location').html(weather_info.city + ", " + weather_info.state);
+	$('.precipitation').html("Precipitation: " + weather_info.precip_today_in + " in");
+	$('.wind').html("Wind: " + weather_info.wind_mph + " mph");
 }
 
 function getWeatherData(position){
